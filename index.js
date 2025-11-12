15ba1bdbf904a77f0610a100d0c85a7ad7474c24
kawsar-ahmmed9990 <kawsarahmmed9990@gmail.com>
2025-11-12 09:43:23 +0600

    add basic crud operation

--- index.js
+++ index.js
@@ -4,20 +4,13 @@ const port = process.env.PORT || 3000;
 
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 
 //middleware
 app.use(cors());
 app.use(express.json());
 
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   },
-// });
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -30,6 +23,50 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
+
+    const db = client.db("traveleaseDB");
+    const vehiclesCollection = db.collection("vehicles");
+
+    app.get("/vehicles", async (req, res) => {
+      const cursor = vehiclesCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/vehicles/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await vehiclesCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.post("/vehicles", async (req, res) => {
+      const newVehicles = req.body;
+      const result = await vehiclesCollection.insertOne(newVehicles);
+      res.send(result);
+    });
+
+    app.patch("/vehicles/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedVehicle = req.body;
+      const query = { _id: new ObjectId(id) };
+      const update = {
+        $set: {
+          name: updatedVehicle.name,
+          price: updatedVehicle.price,
+        },
+      };
+
+      const result = await vehiclesCollection.updateOne(query, update);
+      res.send(result);
+    });
+
+    app.delete("/vehicles/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await vehiclesCollection.deleteOne(query);
+      res.send(result);
+    });
 
     await client.db("admin").command({ ping: 1 });
     console.log(
